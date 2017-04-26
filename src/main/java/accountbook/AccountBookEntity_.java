d38dd009b5ac254b18cb62aeac85b8026e792b49
dadainu <ttnwbca@gmail.com>
2017-04-26 11:37:36 +0900

    家計簿本格開始

--- src/main/java/accountbook/AccountBookEntity_.java
+++ src/main/java/accountbook/AccountBookEntity_.java
@@ -4,12 +4,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value = "Dali", date = "2016-11-05T23:55:49.187+0900")
+@Generated(value="Dali", date="2017-04-26T11:29:41.140+0900")
 @StaticMetamodel(AccountBookEntity.class)
 public class AccountBookEntity_ {
-
-    public static volatile SingularAttribute<AccountBookEntity, Integer> id;
-    public static volatile SingularAttribute<AccountBookEntity, String> registrationDate;
-    public static volatile SingularAttribute<AccountBookEntity, String> uses;
-    public static volatile SingularAttribute<AccountBookEntity, Integer> money;
+	public static volatile SingularAttribute<AccountBookEntity, Integer> id;
+	public static volatile SingularAttribute<AccountBookEntity, String> registrationDate;
+	public static volatile SingularAttribute<AccountBookEntity, String> uses;
+	public static volatile SingularAttribute<AccountBookEntity, Integer> money;
 }
